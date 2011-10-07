ddeb6d568b1465eb017286171317256f8cd043b8
Bruno Carvalho <bmentges@gmail.com>
2011-10-06 22:48:59 -0300

    installation mechanism of the library and ignoring the build dir

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='django-cart',
+      version='1.0.0',
+      description='Django simple shopping cart',
+      maintainer='Bruno Carvalho',
+      maintainer_email='bmentges@gmail.com',
+      url='https://github.com/bmentges/django-cart',
+      packages=['cart'],
+     )
