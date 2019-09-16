ee7a9bea43aa8b2d4eb61a07dec2169492abc5b4
Manasseh Mmadu <mmadumanasseh@gmail.com>
2019-09-16 02:11:36 +0100

    Updated setup.py file to add README to pypi display

--- setup.py
+++ setup.py
@@ -6,10 +6,15 @@ from setuptools import setup
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
         name='django-cart',
         version='1.0.4',
         description='Django simple shopping cart, tests and south migrations included',
+        long_description=long_description,
+        long_description_content_type="text/markdown",
         maintainer='Bruno Carvalho',
         maintainer_email='bmentges@gmail.com',
         license="GNU v3",
