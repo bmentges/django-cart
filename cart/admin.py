555caae8ebfc218cb7ef7a03ff8bf11a502e1230
Bruno Mentges <bmentges@gmail.com>
2026-03-24 17:16:29 -0300

    revamp, updated to Django 4.2+

---
+++ cart/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from .models import Cart, Item
+
+
+class ItemInline(admin.TabularInline):
+    model = Item
+    extra = 0
+    readonly_fields = ("content_type", "object_id", "unit_price", "quantity", "total_price")
+
+    def total_price(self, obj):
+        return obj.total_price
+    total_price.short_description = "Total"
+
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ("id", "creation_date", "checked_out", "item_count")
+    list_filter = ("checked_out",)
+    inlines = [ItemInline]
+
+    def item_count(self, obj):
+        return obj.items.count()
+    item_count.short_description = "Items"
--- runtests.py
+++ runtests.py
@@ -1,14 +1,41 @@
 #!/usr/bin/env python
-import os
+"""
+Standalone test runner. Run from the repository root:
+
+    python runtests.py
+    python runtests.py tests.test_cart.CartAddTest
+"""
 import sys
-from django.core.management import execute_from_command_line
+import django
+from django.conf import settings
+from django.test.utils import get_runner
 
+if not settings.configured:
+    settings.configure(
+        DATABASES={
+            "default": {
+                "ENGINE": "django.db.backends.sqlite3",
+                "NAME": ":memory:",
+            }
+        },
+        INSTALLED_APPS=[
+            "django.contrib.contenttypes",
+            "django.contrib.auth",
+            "cart",
+        ],
+        USE_TZ=True,
+        DEFAULT_AUTO_FIELD="django.db.models.AutoField",
+    )
 
-def runtests():
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tests.settings")
-    argv = sys.argv[:1] + ['test'] + sys.argv[1:]
-    execute_from_command_line(argv)
+django.setup()
 
+# Register FakeProduct so ContentType can resolve it
+from tests.test_cart import FakeProduct  # noqa: E402  (must come after setup)
+from django.contrib.contenttypes.models import ContentType  # noqa: E402
 
-if __name__ == '__main__':
-    runtests()
+if __name__ == "__main__":
+    TestRunner = get_runner(settings)
+    test_runner = TestRunner(verbosity=2, failfast=False)
+    module = sys.argv[1] if len(sys.argv) > 1 else "tests"
+    failures = test_runner.run_tests([module])
+    sys.exit(bool(failures))
