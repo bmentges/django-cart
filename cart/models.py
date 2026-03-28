6f5ffbf6761d4c412a628fdc000620289020885d
Bruno Mentges <bmentges@gmail.com>
2026-03-28 20:28:46 -0300

    Fix deprecated get_object_for_this_type and optimize admin queries
    - Replace deprecated ContentType.get_object_for_this_type() with
      self.content_type.model_class().objects.get(pk=self.object_id) in Item.product property
    - Remove total_price from ItemInline.readonly_fields since it's a computed
      property and causes unnecessary DB queries per row in the admin
    - Bump version to 2.2.2

--- cart/models.py
+++ cart/models.py
@@ -78,7 +78,7 @@ class Item(models.Model):
 
     @property
     def product(self):
-        return self.content_type.get_object_for_this_type(pk=self.object_id)
+        return self.content_type.model_class().objects.get(pk=self.object_id)
 
     @product.setter
     def product(self, product):
